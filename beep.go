80940ca95698e6f84e079067ff6bf8b6a491a129
Flaque <econrad@zagmail.gonzaga.edu>
2018-04-12 20:57:34 -0700

    Add a bit of walking

--- beep.go
+++ beep.go
@@ -6,21 +6,36 @@ import (
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/flaque/beep/parser"
+	s "github.com/golang-collections/collections/stack"
 )
 
-type TreeShapeListener struct {
-	*parser.BaseBeepBoopListener
+var stack *s.Stack
+
+type BeepBoopVisitor struct {
+	*parser.BaseBeepBoopVisitor
 }
 
-func NewTreeShapeListener() *TreeShapeListener {
-	return new(TreeShapeListener)
+func (v *BeepBoopVisitor) Visit(tree antlr.ParseTree) interface{} {
+	fmt.Println("Visit")
+
+	return tree.Accept(v)
 }
 
-func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+func (v *BeepBoopVisitor) VisitMath(ctx *parser.MathContext) interface{} {
+	fmt.Println("Math")
+
+	return v.VisitChildren(ctx) // TODO: Change to v.Visit(INT) + v.VISIT(INT) or something
+}
+
+func (v *BeepBoopVisitor) VisitTerm(ctx *parser.TermContext) interface{} {
+	fmt.Println("Term")
+
+	return v.VisitChildren(ctx)
 }
 
 func main() {
+	stack = s.New()
+
 	input, _ := antlr.NewFileStream(os.Args[1])
 	lexer := parser.NewBeepBoopLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
@@ -30,5 +45,6 @@ func main() {
 	p.BuildParseTrees = true
 	tree := p.Math()
 
-	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+	visitor := BeepBoopVisitor{&parser.BaseBeepBoopVisitor{}}
+	visitor.Visit(tree)
 }
